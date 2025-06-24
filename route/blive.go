7fba8e762f5585ccb6d86e55c3ff95bdbaedc1f2
hirrao <git@hirrao.com>
2025-06-24 23:49:18 +0800

    删除all参数 (#5)

--- route/blive.go
+++ route/blive.go
@@ -26,19 +26,16 @@ type url struct {
 func RouteBLive(app *fiber.App) {
 	group := app.Group("/blive")
 	group.Get("/:roomId", func(ctx *fiber.Ctx) error {
-		res, err := getBliveUrl(ctx.Params("roomId"), ctx.QueryInt("all", 0))
+		res, err := getBliveUrl(ctx.Params("roomId"))
 		if ctx.QueryInt("r", 0) == 1 {
-			uri, ok := res.(string)
-			if ok != true {
-				return http.SendResponse(res, err, ctx)
-			}
+			uri := res[0].Url
 			return ctx.Redirect(uri, fiber.StatusFound)
 		}
 		return http.SendResponse(res, err, ctx)
 	})
 }
 
-func getBliveUrl(roomId string, all int) (interface{}, error) {
+func getBliveUrl(roomId string) ([]url, error) {
 	resp, err := http.Get(
 		fmt.Sprintf("https://api.live.bilibili.com/xlive/web-room/v1/index/getRoomPlayInfo?room_id=%s&play_url=1&mask=1&qn=20000&platform=web", roomId),
 	)
@@ -60,8 +57,5 @@ func getBliveUrl(roomId string, all int) (interface{}, error) {
 	if b.Data.PlayUrl.Durl == nil {
 		return nil, errors.New("无法获取, 可能未开播")
 	}
-	if all == 1 {
-		return b.Data.PlayUrl.Durl, nil
-	}
-	return b.Data.PlayUrl.Durl[1].Url, nil
+	return b.Data.PlayUrl.Durl, nil
 }
